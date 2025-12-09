5f152b008835e7dfe8bccb984aab8bf768555deb
Zane <zanegloh@gmail.com>
2025-12-09 12:07:05 -0800

    add post to create notes

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 # simple boiler plate app.py 
 
-from flask import Flask, jsonify 
+from flask import Flask, request, jsonify 
 
 app = Flask(__name__)
 
 # for now just 
 notes = []
 # notes = ["first note", "second note"]
-next_id = 1 #
+next_id = 0 #
 
 @app.get("/notes")
 def get_notes():
     return jsonify(notes)
+
+@app.post("/notes")
+def create_note():
+    global next_id # declare that you are about to modify the global variable
+    data = request.get_json()
+    text = data.get("text", "")
+    note = {"id": next_id, "text": text}
+    notes.append(note)
+    next_id += 1
+
+    return jsonify(note), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
